8a7fcb47e2e64a5ac2341f32506696a256859bf8
billsibs <billsibs@gmail.com>
2018-06-10 21:18:30 -0400

    Added comments to scripts

--- Assignment03/assignment3one.py
+++ Assignment03/assignment3one.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
+#Prompt the user for a number (x) and then generate x random numbers. 
+#The random numbers should be in the range of 1 to 1000
+#Output the list of the numbers to a text file called numbers.txt
+
+#Import modules. Added argv for fun so script can be run without prompting
 from sys import argv
 from random import randrange
 
+#Variable declaration
 filename = 'numbers.txt'
 
+#Logic to see if user provided value at execution, if not then prompt for input
 if len(argv) > 1:
 	x, gen_count = argv
 else:
 	gen_count = int(input("How many random numbers? "))
 
+#Open file, generate random numbers, write to file, and finally close
 num_file = open(filename,'w')
 for n in range(0, int(gen_count)):
 	num_file.write(str(randrange(1,1000)) + "\n")
--- Assignment03/assignment3two.py
+++ Assignment03/assignment3two.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 
+#Read in the file (numbers.txt) from previous script
+#Sum only the even numbers
+#Output the sum of the even numbers to a new file called num.txt
+
+#Declare variables.
 input_file = 'numbers.txt'
 output_file = 'sum.txt'
 sum_nums = 0
 
+#Open input file
 num_file = open(input_file)
 
+#Use modulus to check if odd or even then add the values into a single variable
 for number in num_file:
 	temp = int(number) % 2
 	if temp == 0:
 		print(number)
 		sum_nums = sum_nums + int(number)
-
+#Close input file
 num_file.close()
+#Open output file
 out_file = open(output_file, 'w')
-
+#Print message and then write to output file
 message = f'The sum of the even numbers in {input_file} is: {sum_nums}\n'
-#print(message)
+#print(message) #Debug line
 out_file.write(message)
-
+#Close output file
 out_file.close()
