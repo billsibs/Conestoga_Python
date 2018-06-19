7da3e07117f77dab35036b9214683d2e78b25f28
billsibs <billsibs@gmail.com>
2018-06-19 08:56:52 -0400

    added filewriter class

--- Assignment04/hrc.py
+++ Assignment04/hrc.py
@@ -11,3 +11,13 @@ class HeartRateCalculator():
 
     def max_target_heart_rate(self):
         return (self.max_heart_rate() * 0.85)
+
+class FileWriter():
+
+	def __init__(self,filename):
+		self.file = filename
+
+	def scribble(self,some_text):
+		f = open(self.file,'a')
+		f.write(some_text)
+		f.close()
